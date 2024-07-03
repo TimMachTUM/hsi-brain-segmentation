4f4ec19ce3932506e0dea8a30c0c65cddeb1406e
Tim Mach <tim.mach@tum.de>
2024-07-03 13:48:57 +0200

    added logs on precision and deciding best model based on precision

--- segmentation_util.py
+++ segmentation_util.py
@@ -19,7 +19,7 @@ def train_and_validate(model, trainloader, validationloader, criterion, optimize
         train_losses, val_losses: List of losses per epoch
     """
     train_losses, val_losses = [], []
-    highest_dice = 0
+    highest_precision = 0
     wandb.watch(model, criterion, log="all", log_freq=10)
     
     for epoch in range(epochs):
@@ -60,10 +60,10 @@ def train_and_validate(model, trainloader, validationloader, criterion, optimize
         val_loss = val_running_loss / len(validationloader)
         val_losses.append(val_loss)
 
-        _,_,_,_, dice_score = evaluate_model(model, validationloader, device, with_wandb=False)
+        precision,_,_,_, dice_score = evaluate_model(model, validationloader, device, with_wandb=False)
         if model_name:
-            if dice_score > highest_dice:
-                highest_dice = dice_score
+            if precision > highest_precision:
+                highest_precision = precision
                 torch.save(model.state_dict(), f'./models/{model_name}.pth')
                 model_artifact = wandb.Artifact(f"{model_name}", type="model")
                 model_artifact.add_file(f'./models/{model_name}.pth')
@@ -259,7 +259,8 @@ def train_sweep(config=None):
 
             print(f'Epoch {epoch+1}, Validation Loss: {val_loss:.4f}')
             wandb.log({"epoch":epoch+1, "validation/loss": val_loss}, step=epoch+1)
-            _,_,_,_, dice_score = evaluate_model(model, testloader, device, with_wandb=False)
+            precision,_,_,_, dice_score = evaluate_model(model, testloader, device, with_wandb=False)
+            wandb.log({"epoch":epoch+1, "precision": precision}, step=epoch+1)
             wandb.log({"epoch":epoch+1, "dice_score": dice_score}, step=epoch+1)
         
         del model
